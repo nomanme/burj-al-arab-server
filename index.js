2bcf7c34e0a1d54270ce5cb9496f401c97421473
nomanme <abdullah.noman083@gmail.com>
2020-10-04 00:10:02 +0600

    fixed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const admin = require('firebase-admin');
 require('dotenv').config()
 // console.log(process.env.DB_PASS)
-const port = 6000;
+const port = 7000;
 
 // app.get('/', (req, res) => {
 //     res.send('hello mongodb')
@@ -22,8 +22,6 @@ admin.initializeApp({
     databaseURL: process.env.FIRE_DB
 });
 
-
-
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w8usp.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -40,36 +38,40 @@ client.connect(err => {
             })
         console.log(newBooking);
     })
+
+    app.get('/bookings', (req, res) => {
+        const bearer = req.headers.authorization;
+        console.log(bearer)
+        const queryEmail = req.query.email;
+        bookings.find({ email: queryEmail })
+            .toArray((err, documents) => {
+                return res.status(200).send(documents);
+            })
+
+        // if (bearer && bearer.startsWith('Bearer ')) {
+        //     const idToken = bearer.split(' ')[1];
+        //     console.log(idToken)
+        //     admin.auth().verifyIdToken(idToken)
+        //         .then(function (decodedToken) {
+        //             const tokenEmail = decodedToken.email;
+        //             console.log(queryEmail === tokenEmail)
+        //             if (tokenEmail == queryEmail) {
+
+        //             }
+        //             else {
+        //                 res.status(401).send('un-authorized access')
+        //             }
+        //         }).catch(function (error) {
+        //             res.status(401).send('un-authorized access')
+        //         });
+        // }
+        // else {
+        //     res.status(401).send('un-authorized access')
+        // }
+    })
 })
 
-app.get('/bookings', (req, res) => {
-    const bearer = req.headers.authorization;
-    console.log(bearer)
-    if (bearer && bearer.startsWith('Bearer ')) {
-        const idToken = bearer.split(' ')[1];
-        console.log(idToken)
-        admin.auth().verifyIdToken(idToken)
-            .then(function (decodedToken) {
-                const tokenEmail = decodedToken.email;
-                const queryEmail = req.query.email;
-                console.log(queryEmail === tokenEmail)
-                if (tokenEmail == queryEmail) {
-                    bookings.find({ email: queryEmail })
-                        .toArray((err, documents) => {
-                            res.status(200).send(documents);
-                        })
-                }
-                else {
-                    res.status(401).send('un-authorized access')
-                }
-            }).catch(function (error) {
-                res.status(401).send('un-authorized access')
-            });
-    }
-    else {
-        res.status(401).send('un-authorized access')
-    }
-})
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
